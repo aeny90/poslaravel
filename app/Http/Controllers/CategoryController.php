e67d9f1c9d6cb832265b76624eabdcc000628b50
aeny90 <aenybista@gmail.com>
2020-03-06 11:23:41 +0700

    first commit

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Category;
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        $categories = Category::orderBy('created_at', 'DESC')->paginate(10);
+        return view('categories.index', compact('categories'));
+    }
+    public function store(Request $request){
+    	//validasi form
+    	$this->validate($request,[
+    		'name' => 'required|string|max:50',
+    		'description' => 'nullable|string'
+	    	]);
+	    try{
+	    	$categories = Category::firstOrCreate([
+	    		'name'=>$request->name
+		    ],[
+		    	'description'=>$request->description
+		    ]);
+		    return redirect()->back()->with(['success' => 'Kategori:' . $categories->name . 'Ditambahkan']);
+	    } catch(\Exception $e){
+	    	return redirect()->back()->with(['error'=>$e->getMessage()]);
+	    }
+    }
+}
